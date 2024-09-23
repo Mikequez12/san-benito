816009d5a1dea2ccf852814c5244cf2a51b626c8
Mike <142720633+Mikequez12@users.noreply.github.com>
2024-09-23 19:21:26 +0200

    Delete verification.js

--- verification.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded',(DOMEvent) => {
-	window.URLVariables = getURLVariables();
-	console.log(window.URLVariables);
-	if (window.URLVariables === null || URLVariables.group === null || URLVariables.course === null) {
-		window.location.href = `${window.project_dir}/login?redirect=${encodeURI(window.location.href.split('?')[0])}`;
-	}
-	console.log(window.location.href)
-})
