ff5cd7609e992eea11e5b93bf7fc51103c5258a7
Mike <142720633+Mikequez12@users.noreply.github.com>
2024-10-21 23:30:29 +0200

    Update verification.js

--- alpha/verification.js
+++ alpha/verification.js
@@ -1,13 +1,16 @@
 document.addEventListener('DOMContentLoaded',(DOMEvent) => {
 	window.URLVariables = getURLVariables();
 	console.log(getURLVariables());
-	let customcol = getURLVariables().customcol;
-	if (customcol === undefined) {
-		customcol = '';
-	} else {
-		customcol = `customcol=${customcol}&`;
+	let customcol = '';
+	if (getURLVariables() !== null) {
+		customcol = getURLVariables().customcol;
+		if (customcol === undefined) {
+			customcol = '';
+		} else {
+			customcol = `customcol=${customcol}&`;
+		};
 	};
-	if (getURLVariables().course === undefined || getURLVariables().group === undefined) {
+	if (getURLVariables() === null || getURLVariables().course === undefined || getURLVariables().group === undefined) {
 		window.location.href = `${window.project_dir}/login?${customcol}redirect=${encodeURI(window.location.href)}`;
 	}
 	console.log(window.location.href)
