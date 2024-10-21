0a84db164238d5f76339bcbf1c0bfdd94d5bbc0d
Mike <142720633+Mikequez12@users.noreply.github.com>
2024-10-21 23:24:48 +0200

    Add files via upload

--- alpha/requests/spreadsheets.js
+++ alpha/requests/spreadsheets.js
@@ -1,1 +1,19 @@
+let page = document.currentScript.getAttribute('dataload');
+const url = `https://sheets.googleapis.com/v4/spreadsheets/10XrDFPdMcVkcHkHtotobIjpS2o586hGTzw1L--wPY60/values/${page}?key=AIzaSyCPoCo9JcBf6_p7JqlPDZ_6frBODdw4EAI`;
 
+console.log(url)
+
+fetch(url)
+    .then(response => {
+        return response.json();
+        if (!response.ok) {
+            throw new Error('ERROR IN LOAD');
+        }
+    })
+    .then(data => {
+    	if (window.loaded_data === undefined) {
+    		window.loaded_data = {};
+    	};
+        window.loaded_data[page]= data;
+    })
+    .catch(error => console.error('Error:', error));
