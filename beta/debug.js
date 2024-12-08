973be86b362f2c1110f75b5e9458a7a9d035fdc4
Mike <142720633+Mikequez12@users.noreply.github.com>
2024-12-08 20:48:45 +0100

    remove beta 1.0

--- beta/debug.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', (DOMEvent) => {
-	document.querySelectorAll('*[debug]').forEach((element) => {
-		console.log(element);
-		console.log(element.getAttribute('debug'));
-		element.style.background = `${element.getAttribute('debug')}`;
-		console.log(element.style.background);
-	})
-})
--- beta/index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded',(DOMEvent) => {
-	let egutegia = document.querySelector('#Egutegia');
-	let today = new Date();
-	let year = today.getFullYear() - (today.getMonth() < 5);
-	let img = egutegia.appendChild(document.createElement('img'));
-	img.src=`https://www.sanbenitoikastola.eus/sites/default/files/Egutegiak/egutegiak_ikasleak_${year}-${String(year+1).slice(2)}_1-1.png`;
-	img.style.width = '100%';
-})
