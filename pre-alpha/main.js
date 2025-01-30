d0f7a798ab5c95698561d1571e7043cd27a90cbd
Mike <142720633+Mikequez12@users.noreply.github.com>
2025-01-30 19:10:59 +0100

    w

--- pre-alpha/main.js
+++ pre-alpha/main.js
@@ -1,6 +1,6 @@
 window.project_dir = 'https://mikequez12.github.io/san-benito/pre-alpha';
 if (getURLVariables().force!='true') {
-    indow.location.href = window.location.href.replace('pre-alpha',window.app_version);
+    window.location.href = window.location.href.replace('pre-alpha',window.app_version);
 }
 
 document.addEventListener('DOMContentLoaded',(DOMEvent) => {
