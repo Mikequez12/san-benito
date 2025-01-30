339e5dc8cf99c970c6742edc1b6d3dc536c60f62
Mike <142720633+Mikequez12@users.noreply.github.com>
2025-01-30 23:00:32 +0100

    force alpha

--- alpha/login/login.js
+++ alpha/login/login.js
@@ -2,11 +2,13 @@ function login() {
 	let a = document.querySelector('#course').value;
 	let b = document.querySelector('#group').value;
 
+	let force = getURLVariables().force=='true'?'&force=true':'';
+
 	let url = decodeURIComponent(
 		window.location.href.split('?').slice(1).join('?').split('redirect=')[1]
 	)
 
-	url += `${url.includes('?')?'&':'?'}course=${a}&group=${b}`;
+	url += `${url.includes('?')?'&':'?'}course=${a}&group=${b}${force}`;
 
 	window.location.href = url;
 }
