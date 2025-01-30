fdcbeba723009fb0bf9fec69c0064618b7fb089c
Mike <142720633+Mikequez12@users.noreply.github.com>
2025-01-30 22:08:17 +0100

    login updated

--- pre-alpha/login/login.js
+++ pre-alpha/login/login.js
@@ -1,9 +1,11 @@
 function login() {
 	let a = document.querySelector('#course').value;
 	let b = document.querySelector('#group').value;
+	let force = '';
+	if (getURLVariables().force == 'true') force = '&force=true'
 	window.location.href = (
 		decodeURIComponent(
 			window.location.href.split('?')[1].split('redirect=')[1]
-		)+`?course=${a}&group=${b}`
+		)+`?course=${a}&group=${b}${force}`
 	);	
 }
