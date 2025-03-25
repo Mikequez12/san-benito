38445e6bb0f01ab8bac2429f040f7a653fb88551
Mike <142720633+Mikequez12@users.noreply.github.com>
2025-03-25 12:00:29 +0100

    update

--- beta/index.js
+++ beta/index.js
@@ -3,6 +3,6 @@ document.addEventListener('DOMContentLoaded',(DOMEvent) => {
 	let today = new Date();
 	let year = today.getFullYear() - (today.getMonth() < 5);
 	let img = egutegia.appendChild(document.createElement('img'));
-	img.src=`https://sanbenitoikastola.eus/sites/default/files/pdf_dokumentuak/${year}-01/egutegiak_ikasleak_${year}-${String(year+1).slice(2)}_1.pdf`;
+	img.src=`https://sanbenitoikastola.eus/sites/default/files/pdf_dokumentuak/${year+1}-01/egutegiak_ikasleak_${year}-${String(year+1).slice(2)}_1.pdf`;
 	img.style.width = '100%';
 })
