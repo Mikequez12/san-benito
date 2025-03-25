74487eaaf5971a8a45ddadf4cb59da03d126d08c
Mike <142720633+Mikequez12@users.noreply.github.com>
2025-03-25 12:03:04 +0100

    update

--- beta/index.js
+++ beta/index.js
@@ -2,7 +2,11 @@ document.addEventListener('DOMContentLoaded',(DOMEvent) => {
 	let egutegia = document.querySelector('#Egutegia');
 	let today = new Date();
 	let year = today.getFullYear() - (today.getMonth() < 5);
-	let img = egutegia.appendChild(document.createElement('img'));
+	let img = egutegia.appendChild(document.createElement('iframe'));
+	img.style.border = 'none';
+	img.style.outline = 'none';
+	img.style.width = '100%';
+	img.style.height = '100%';
 	img.src=`https://sanbenitoikastola.eus/sites/default/files/pdf_dokumentuak/${year+1}-01/egutegiak_ikasleak_${year}-${String(year+1).slice(2)}_1.pdf`;
 	img.style.width = '100%';
 })
